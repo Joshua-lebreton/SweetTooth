f0902ee10e4e8b145e00ea916485a4b5a8fc9fb5
Joshua Lebreton <200495149@student.georgianc.on.ca>
2022-09-15 17:03:49 -0400

    Added catergory modle class

---
+++ SweetTooth/Controllers/Categories.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SweetTooth.Models;
+
+namespace SweetTooth.Controllers
+{
+    public class Categories : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = new List<Category>();
+            for (int i = 1; i <= 10; i++)
+                categories.Add(new Category { CategoryId = i, Name = "Category" + i });
+            return View(categories);
+        }
+
+        public IActionResult Browse(string category) 
+        { 
+            ViewData["category"] = category;
+            return View();
+        }
+    }
+}
---
+++ SweetTooth/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SweetTooth.Models
+{
+    public class Category
+    {
+        //all primary key fields should be called ID or the class name followed by Id (CategoryId)
+        [Required]
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
